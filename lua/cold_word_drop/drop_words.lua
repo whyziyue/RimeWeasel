362065ad2add1bc1ca325540363646d9039a2f70
whyziyue <93143477+whyziyue@users.noreply.github.com>
2024-10-23 12:59:59 +0800

    解决重新部署或开机后被删除候选词自动恢复的问题

--- lua/cold_word_drop/drop_words.lua
+++ lua/cold_word_drop/drop_words.lua
@@ -1,3 +1,5 @@
 local drop_words =
-{ "示~例~", "肏女人", }
+{ "示~例~", "雄风", "颜射", "内射", "爆乳"
+}
 return drop_words
+-- 事实上删除候选词只需要在drop_words.lua中增加词语
--- lua/cold_word_drop/filter.lua
+++ lua/cold_word_drop/filter.lua
@@ -1,14 +1,22 @@
 local filter = {}
 
+-- 初始化，加载配置和删除词条记录
 function filter.init(env)
 	local engine = env.engine
 	local config = engine.schema.config
 	env.word_reduce_idx = config:get_int("cold_word_reduce/idx") or 4
-	env.drop_words = require("cold_word_drop.drop_words") or {}
-	env.hide_words = require("cold_word_drop.hide_words") or {}
-	env.reduce_freq_words = require("cold_word_drop.reduce_freq_words") or {}
+
+	-- 加载删词和隐藏词条的持久化记录
+	env.drop_words = require("cold_word_drop.drop_words")
+	env.hide_words = require("cold_word_drop.hide_words")
+	env.reduce_freq_words = require("cold_word_drop.reduce_freq_words")
+
+	-- 加载删除和隐藏的词条文件
+	env.drop_words.load()  -- 从文件加载删除的词条
+	env.hide_words.load()  -- 从文件加载隐藏的词条
 end
 
+-- 过滤候选词
 function filter.func(input, env)
 	local cands = {}
 	local context = env.engine.context
@@ -17,13 +25,14 @@ function filter.func(input, env)
 	local hide_words = env.hide_words
 	local word_reduce_idx = env.word_reduce_idx
 	local reduce_freq_words = env.reduce_freq_words
+
 	for cand in input:iter() do
 		local cand_text = cand.text:gsub(" ", "")
 		local preedit_code = cand.preedit:gsub(" ", "") or preedit_str
 
 		local reduce_freq_list = reduce_freq_words[cand_text] or {}
 		if word_reduce_idx > 1 then
-			-- 前三个 候选项排除 要调整词频的词条, 要删的(实际假性删词, 彻底隐藏罢了) 和要隐藏的词条
+			-- 前三个 候选项排除 要调整词频的词条, 已删除的词条 和 要隐藏的词条
 			if reduce_freq_list and table.find_index(reduce_freq_list, preedit_code) then
 				table.insert(cands, cand)
 			elseif
@@ -55,6 +64,13 @@ function filter.func(input, env)
 	for _, cand in ipairs(cands) do
 		yield(cand)
 	end
+end
+
+-- 删除词条并保存到文件
+function filter.delete_word(cand_text)
+	-- 添加到 drop_words 并持久化保存
+	env.drop_words[cand_text] = true
+	env.drop_words.save()  -- 保存到文件
 end
 
 return filter
